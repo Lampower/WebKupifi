b55b5cb6256c5c1888f44071209e70ed7061a693
maxpiratt04 <53815796+maxpiratt04@users.noreply.github.com>
2024-03-28 18:05:00 +0100

    hihihi

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 function bootstrap() {
-  let app;
-  NestFactory.create(AppModule).then((appm) => {
+  let app: any;
+  NestFactory.create(AppModule).then((appm: any) => {
     app = appm;
     app.listen(3000);
   });
