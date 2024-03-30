a593648c6dc11e721ed76849449ef3a887b66162
maxpiratt04 <53815796+maxpiratt04@users.noreply.github.com>
2024-03-30 12:07:29 +0100

    trap

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-function bootstrap() {
+function trap() {
   let app: any;
   NestFactory.create(AppModule).then((appm: any) => {
     app = appm;
     app.listen(3000);
   });
 }
-bootstrap();
+trap();
